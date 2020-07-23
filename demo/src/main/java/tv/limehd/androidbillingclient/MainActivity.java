e79bfc52d3bf2f493b31ad1ae703fdd60ef3cfb8
Terentev Vlad <42178405+TerentevVlad@users.noreply.github.com>
2020-07-23 14:37:27 +0300

    Модуль app переименован в модуль demo

--- demo/src/main/java/tv/limehd/androidbillingclient/MainActivity.java
+++ demo/src/main/java/tv/limehd/androidbillingclient/MainActivity.java
@@ -1,17 +1,7 @@
 package tv.limehd.androidbillingmodule;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
