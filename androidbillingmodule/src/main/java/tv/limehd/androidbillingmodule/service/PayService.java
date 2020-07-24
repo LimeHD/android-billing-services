bea904a28b97d5c627422d343f2667c8c827b8e3
Terentev Vlad <42178405+TerentevVlad@users.noreply.github.com>
2020-07-24 12:50:10 +0300

    Добавлен PayService [#173977365]

---
+++ androidbillingmodule/src/main/java/tv/limehd/androidbillingmodule/service/PayService.java
@@ -0,0 +1,33 @@
+package tv.limehd.androidbillingmodule.service;
+
+import tv.limehd.androidbillingmodule.interfaces.IPayServicesStrategy;
+import tv.limehd.androidbillingmodule.interfaces.listeners.ExistenceServiceListener;
+import tv.limehd.androidbillingmodule.interfaces.listeners.ExistenceServicesListener;
+import tv.limehd.androidbillingmodule.servicesPay.EnumPaymentService;
+import tv.limehd.androidbillingmodule.servicesStrategy.ServiceGoogleStrategy;
+import tv.limehd.androidbillingmodule.servicesStrategy.ServiceHuaweiStrategy;
+
+public class PayService {
+
+    private EnumPaymentService nameService;
+    private IPayServicesStrategy servicesStrategy;
+
+    public PayService(EnumPaymentService paymentService) {
+        nameService = paymentService;
+        servicesStrategy = initServicesStrategyByPayService(nameService);
+    }
+
+    public static IPayServicesStrategy initServicesStrategyByPayService(EnumPaymentService paymentServices) {
+        IPayServicesStrategy iPayServicesStrategy;
+        switch (paymentServices) {
+            case google: iPayServicesStrategy = new ServiceGoogleStrategy(); break;
+            case huawei: iPayServicesStrategy = new ServiceHuaweiStrategy(); break;
+            default: iPayServicesStrategy = null;
+        }
+        return iPayServicesStrategy;
+    }
+
+    public void verifyExistence (ExistenceServiceListener existenceServiceListener) {
+        existenceServiceListener.callBackExistenceService(nameService, servicesStrategy.verifyExistenceService());
+    }
+}
