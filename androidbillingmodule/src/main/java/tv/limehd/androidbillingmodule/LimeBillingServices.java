ff752b0abf8488d2bb66254e9c6bb412ba8cefee
Terentev Vlad <42178405+TerentevVlad@users.noreply.github.com>
2020-07-24 13:04:29 +0300

    Добавлена покупка из сервиса. Поправлены тесты [#173977365]

--- androidbillingmodule/src/main/java/tv/limehd/androidbillingmodule/LimeBillingServices.java
+++ androidbillingmodule/src/main/java/tv/limehd/androidbillingmodule/LimeBillingServices.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import tv.limehd.androidbillingmodule.controllers.ControllerVerifyServices;
 import tv.limehd.androidbillingmodule.interfaces.listeners.ExistenceServicesListener;
 import tv.limehd.androidbillingmodule.service.PayService;
-import tv.limehd.androidbillingmodule.servicesPay.EnumPaymentService;
+import tv.limehd.androidbillingmodule.servicesEnum.EnumPaymentService;
 
 public class LimeBillingServices {
 
@@ -27,10 +27,18 @@ public class LimeBillingServices {
         ControllerVerifyServices controllerVerifyServices = new ControllerVerifyServices(payServices, existenceServicesListener);
     }
 
+    public boolean tryBuySubscriptionFrom(EnumPaymentService nameService) {
+        PayService payService = payServices.get(nameService);
+        if(payService != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     private PayService initServiceByPaymentService(EnumPaymentService paymentService) {
         PayService payService = new PayService(paymentService);
         return payService;
     }
-
-
+    
 }
