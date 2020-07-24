c9723a634da14751bdfd2235c69f7b1469af36fb
Terentev Vlad <42178405+TerentevVlad@users.noreply.github.com>
2020-07-24 14:58:06 +0300

    Отредактирован демо модуль [#173977365]

--- demo/src/main/java/tv/limehd/androidbillingclient/MainActivity.java
+++ demo/src/main/java/tv/limehd/androidbillingclient/MainActivity.java
@@ -1,8 +1,7 @@
 package tv.limehd.androidbillingclient;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,11 +13,18 @@ import tv.limehd.androidbillingmodule.servicesEnum.EnumPaymentService;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button buttonGoogleSDK;
+    private Button buttonHuaweiSDK;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("TEST", "Verify");
+
+        buttonGoogleSDK = findViewById(R.id.button_google_sdk);
+        buttonHuaweiSDK = findViewById(R.id.button_huawei_sdk);
+
+
         LimeBillingServices limeBillingServices = new LimeBillingServices(getApplicationContext())
                 .init();
 
@@ -26,11 +32,11 @@ public class MainActivity extends AppCompatActivity {
         limeBillingServices.verifyExistenceAllService(new ExistenceServicesListener() {
             @Override
             public void callBackExistenceServices(HashMap<EnumPaymentService, Boolean> existing) {
-                for (EnumPaymentService service: EnumPaymentService.values()) {
-                    Log.e("TEST", "service: " + service.name() + " " + existing.get(service));
-                }
-                ((TextView)findViewById(R.id.googleSdk)).setText("service: " + EnumPaymentService.google.name() + " " + existing.get(EnumPaymentService.google));
-                ((TextView)findViewById(R.id.huaweiSdk)).setText("service: " + EnumPaymentService.huawei.name() + " " + existing.get(EnumPaymentService.huawei));
+                buttonGoogleSDK.setEnabled(existing.get(EnumPaymentService.google));
+                buttonGoogleSDK.setText("service: " + EnumPaymentService.google.name());
+
+                buttonHuaweiSDK.setEnabled(existing.get(EnumPaymentService.huawei));
+                buttonHuaweiSDK.setText("service: " + EnumPaymentService.huawei.name());
             }
         });
     }
