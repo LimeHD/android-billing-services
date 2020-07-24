a919d020bf802c09d198fa2026ef08ea46d74f50
Terentev Vlad <42178405+TerentevVlad@users.noreply.github.com>
2020-07-24 09:07:21 +0300

    Добавлены классы для гугл, хуавей и для другой системы [#173977233]

---
+++ androidbillingmodule/src/main/java/tv/limehd/androidbillingmodule/ServicesStrategy/ServiceGoogleStrategy.java
@@ -0,0 +1,10 @@
+package tv.limehd.androidbillingmodule.ServicesStrategy;
+
+import tv.limehd.androidbillingmodule.Interfaces.IPayServicesStrategy;
+
+public class ServiceGoogleStrategy implements IPayServicesStrategy {
+    @Override
+    public void buy() {
+
+    }
+}
---
+++ androidbillingmodule/src/main/java/tv/limehd/androidbillingmodule/ServicesStrategy/ServiceHuaweiStrategy.java
@@ -0,0 +1,10 @@
+package tv.limehd.androidbillingmodule.ServicesStrategy;
+
+import tv.limehd.androidbillingmodule.Interfaces.IPayServicesStrategy;
+
+public class ServiceHuaweiStrategy implements IPayServicesStrategy {
+    @Override
+    public void buy() {
+
+    }
+}
