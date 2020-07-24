08ad1ae263b2333d822692b446dffb081d9eeb13
Terentev Vlad <42178405+TerentevVlad@users.noreply.github.com>
2020-07-24 14:22:38 +0300

    Добавлена проверка на существование huawei сервисов [#173977365]

--- androidbillingmodule/src/main/java/tv/limehd/androidbillingmodule/servicesStrategy/ServiceHuaweiStrategy.java
+++ androidbillingmodule/src/main/java/tv/limehd/androidbillingmodule/servicesStrategy/ServiceHuaweiStrategy.java
@@ -2,6 +2,10 @@ package tv.limehd.androidbillingmodule.servicesStrategy;
 
 import android.content.Context;
 
+import com.huawei.hms.api.ConnectionResult;
+import com.huawei.hms.api.HuaweiApiAvailability;
+import com.huawei.hms.common.HuaweiApi;
+
 import tv.limehd.androidbillingmodule.interfaces.IPayServicesStrategy;
 
 public class ServiceHuaweiStrategy implements IPayServicesStrategy {
@@ -12,7 +16,8 @@ public class ServiceHuaweiStrategy implements IPayServicesStrategy {
 
     @Override
     public boolean verifyExistenceService(Context context) {
-        return false;
+        int status = HuaweiApiAvailability.getInstance().isHuaweiMobileServicesAvailable(context);
+        return status == ConnectionResult.SUCCESS;
     }
 
 }
