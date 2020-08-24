4c9afde2e2e50a20d484ca2d1a8bfae1a04b55a6
andrey <moshaikin.andrei@gmail.com>
2020-08-24 14:32:02 +0300

    изменение гугл стратегии изменение гугл стратегии

--- androidbillingmodule/src/main/java/tv/limehd/androidbillingmodule/service/strategy/google/callBacks/GoogleBuySubscriptionCallBacks.java
+++ androidbillingmodule/src/main/java/tv/limehd/androidbillingmodule/service/strategy/google/callBacks/GoogleBuySubscriptionCallBacks.java
@@ -3,20 +3,13 @@ package tv.limehd.androidbillingmodule.service.strategy.google.callBacks;
 import java.util.Map;
 
 import tv.limehd.androidbillingmodule.service.PurchaseData;
-import tv.limehd.androidbillingmodule.service.strategy.ServiceCallBack;
 
-public interface GoogleCallBacks extends ServiceCallBack {
+public interface GoogleBuySubscriptionCallBacks {
     void onAcknowledgePurchaseStart();
 
     void onPurchaseAcknowledgeSuccess(PurchaseData purchaseData, Map<String, PurchaseData> purchaseDataMap);
 
     void onAcknowledgePurchaseError(String error);
-
-    void onBillingSetupFinishedSuccess();
-
-    void onBillingSetupFinishedError(String message);
-
-    void onBillingServiceDisconnected();
 
     void onPurchaseUpdateError(String message);
 }
